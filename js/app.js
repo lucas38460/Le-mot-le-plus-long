2da5b2507fa2b305ae798261f111710388d7a72e
Lux <92374048+lucas38460@users.noreply.github.com>
2022-05-17 11:56:52 +0200

    fin de l'exo part1

--- js/app.js
+++ js/app.js
@@ -1,27 +1,27 @@
 const mot = ["appartement"];
 
-function blender(word) {
-  const word_blender = word.split(' ');
+function blender([word]) {
+  const word_blender = word.split("");
   if (typeof word === "number") {
+    console.log("mauvaise piste");
     return;
   } else {
     for (let position = word_blender.length - 1; position >= 1; position--) {
       let hasard = Math.floor(Math.random() * (position + 1));
-
       let sauve = word_blender[position];
       word_blender[position] = word_blender[hasard];
       word_blender[hasard] = sauve;
-      console.log(word_blender);
     }
-    return word_blender;
+    return word_blender.join("");
   }
 }
 
-function testes(word_blender) {
-  if (word_blender === mot) {
+function testes(mot) {
+  blender(mot);
+  if (mot === word_blender) {
     blender(mot);
   }
 }
 
-let start = blender(mot);
-console.log(start);
+let rap = blender(mot);
+console.log(rap);
